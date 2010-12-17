4363ea7ccf1561acac7170b7f2b67a1e398ffb3b
Default Virtual Server <jill@ff9qrraa.(none)>
2010-12-17 06:53:27 +0000

    initial import of backu.py... the bones

---
+++ bin/backu.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python -t
+''' backupy
+backs up everything, with configuration sprinkled via .__backupy__ files 
+on the filesystem.
+
+i use gnu versions of the various commands where appropriate.  py2.6.
+'''
